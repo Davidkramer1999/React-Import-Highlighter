62125d547461f29d97a8fe64b7bfccdbbc2b0be8
kramer <david.kramer@gmail.com>
2023-09-13 11:27:32 +0200

    bug with split screen...

--- extension.js
+++ extension.js
@@ -173,105 +173,42 @@ function checkImportsInFiles(dependencies) {
   }
 }
 
-const processedFiles = new Set();
+let isScreenSplit = false;
+let files = [];
+let activeTabGroupsCount = 1;  // Start with a single tab group by default
 
-vscode.window.onDidChangeActiveTextEditor(() => {
-  const activeEditor = vscode.window.activeTextEditor;
-  const activeFilePath = activeEditor?.document?.fileName;
-  const activeFileName = path.basename(activeFilePath);
+const processActiveFile = (fileName) => {
+  // Determine if the screen is split
+  isScreenSplit = vscode.window.tabGroups.all.length > 1;
 
-  console.log("Active Filename:", activeFileName);
-  console.log("Processed Files:", processedFiles);
-  console.log("Decorated inlucdes alreaday?:", processedFiles.has(activeFileName));
-
-  // If the file has already been processed, skip running the code
-  if (processedFiles.has(activeFileName)) {
-    return;
-  }
-
-  const dependencies = dependencyCache.getDependenciesFromPackageJson();
-  if (dependencies) {
-    checkImportsInFiles(dependencies);
-  }
-});
-
-//when opening a new file
-vscode.window.tabGroups.onDidChangeTabGroups((event) => {
-  const activeTabGroup = vscode.window.tabGroups.activeTabGroup;
-  const isActive = activeTabGroup?.isActive;
-
-  if (isActive) {
-    const activeFileName = activeTabGroup?.activeTab?.label;
-
-    // Add the activeFileName to the processedFiles Set
-    if (activeFileName) {
-      processedFiles.add(activeFileName);
-    }
-
-    // Your code to apply decorations or any other operation
-  } else {
-    processedFiles.clear();
-  }
-});
-
-/*try {
-  vscode.window.tabGroups.onDidChangeTabGroups((event) => {
-    try {
-      console.log("Tab Groups changed:", event);
-      const activeTabGroup = vscode.window.tabGroups.activeTabGroup;
-      const activeEditor = activeTabGroup?.activeEditor;
-
-      // Get the URI or fileName from the activeEditor
-      const activeFileName = activeEditor?.document?.fileName;
-
-      console.log("Active Filename:", activeFileName);
-    } catch (innerError) {
-      console.error("Error in event handler:", innerError);
-    }
-  });
-} catch (error) {
-  console.error("Error setting up event listener:", error);
-}*/
-
-//const decoratedFiles = new Set();
-
-/*vscode.workspace.onDidChangeTabGroups(() => {
-  console.log("Active Filename:", activeFileName);
-  console.log("Decorated Files:", decoratedFiles);
-  const activeTabGroup = vscode.workspace.tabGroups.activeTabGroup;
-  const activeEditor = activeTabGroup?.activeEditor;
-  console.log("Active Filename:", activeFileName);
-  console.log("Decorated Files:", decoratedFiles);
-  // Get the URI or fileName from the activeEditor
-  const activeFileName = activeEditor?.document?.fileName;
-
-  if (activeFileName) {
-    if (decoratedFiles.has(activeFileName)) {
-      // The file is already decorated, do not reapply decorations
-      return;
-    }
-    // Apply your decorations
-    // applyDecorations(activeEditor);
-
-    // Add the file to the set of decorated files
-    decoratedFiles.add(activeFileName);
-  }
-});
-
-/*function applyDecorations(editor) {
-  if (editor) {
+  if (!files.includes(fileName)) {
+    files.push(fileName);
     const dependencies = dependencyCache.getDependenciesFromPackageJson();
     if (dependencies) {
       checkImportsInFiles(dependencies);
     }
   }
-}*/
+};
+
+vscode.window.onDidChangeActiveTextEditor((event) => {
+  const activeFileName = event.document.fileName;
+  processActiveFile(activeFileName);
+});
+
+vscode.window.tabGroups.onDidChangeTabGroups((event) => {
+  isScreenSplit = true;
+  activeTabGroupsCount = vscode.window.tabGroups.all.length;
+
+  if (activeTabGroupsCount === 1) {
+    files = [];
+    isScreenSplit = false;
+  }
+});
+
+
 
 const performCheck = () => {
-  const dependencies = dependencyCache.getDependenciesFromPackageJson();
-  if (dependencies) {
-    checkImportsInFiles(dependencies);
-  }
+  processActiveFile();
 };
 
 function activate(context) {
@@ -280,6 +217,6 @@ function activate(context) {
   context.subscriptions.push(disposable);
 }
 
-function deactivate() {}
+function deactivate() { }
 
 module.exports = { activate, deactivate };
