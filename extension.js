d97581b5a14fb1ca79535c0f28b1c5ba40d45011
kramer <david.kramer@gmail.com>
2023-09-24 13:29:24 +0200

    optimization

--- extension.js
+++ extension.js
@@ -3,40 +3,48 @@ const { initializeReactImportHighlighter } = require('./src/core/initializeReact
 const dependencyCaches = require('./src/utils/getDependencyCache');
 const { highlighterSettings } = require('./src/utils/highlighter');
 
-function activate() {
-  const dependencyCache = dependencyCaches.getDependencyCache();
-
-  if (!dependencyCache) {
-    console.error("DependencyCache is not available");
-    return;
+class DependencyCacheHandler {
+  constructor(dependencyCache) {
+    this.dependencyCache = dependencyCache;
+    this.cacheInitialized = false;
   }
 
-  setupDependencyCacheInitializer(dependencyCache);
-  executeInitializer(dependencyCache);
-  subscribeToCacheUpdates(dependencyCache);
-}
-
-function setupDependencyCacheInitializer(dependencyCache) {
-  let cacheInitialized = false;
-
-  dependencyCache.setInitializer(() => {
-    if (!cacheInitialized) {
-      console.log("Initializer called");
-      initializeReactImportHighlighter();
-      cacheInitialized = true;
+  setup() {
+    if (!this.dependencyCache) {
+      console.error("DependencyCache is not available");
+      return;
     }
-  });
+    this.setupInitializer();
+    this.executeInitializer();
+    this.subscribeToCacheUpdates();
+  }
+
+  setupInitializer() {
+    this.dependencyCache.setInitializer(() => {
+      if (!this.cacheInitialized) {
+        console.log("Initializer called");
+        initializeReactImportHighlighter();
+        this.cacheInitialized = true;
+      }
+    });
+  }
+
+  executeInitializer() {
+    this.dependencyCache.initializer();
+  }
+
+  subscribeToCacheUpdates() {
+    this.dependencyCache.on('cacheUpdated', () => {
+      highlighterSettings.highlightDecorationType.dispose();
+      initializeReactImportHighlighter();
+    });
+  }
 }
 
-function executeInitializer(dependencyCache) {
-  dependencyCache.initializer();
-}
-
-function subscribeToCacheUpdates(dependencyCache) {
-  dependencyCache.on('cacheUpdated', () => {
-    highlighterSettings.highlightDecorationType.dispose();
-    initializeReactImportHighlighter();
-  });
+function activate() {
+  const dependencyCache = dependencyCaches.getDependencyCache();
+  const handler = new DependencyCacheHandler(dependencyCache);
+  handler.setup();
 }
 
 function deactivate() {
--- src/utils/getDependencyCache.js
+++ src/utils/getDependencyCache.js
@@ -3,6 +3,6 @@ const { DependencyCache } = require('../classes/DependencyCache');
 const dependencyCache = new DependencyCache();
 
 module.exports = {
-    getDependencyCache: () => dependencyCache
+    getDependencyCache: () => dependencyCache,
+    getDependencies: () => dependencyCache.getDependenciesFromPackageJson()
 };
-
